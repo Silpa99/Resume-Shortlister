49856d5bd36e652387e0c47921ecfa9405fbf8b2
Silpa <silpats99@gmail.com>
2020-07-05 00:01:00 +0530

    ..

--- web_app/views.py
+++ web_app/views.py
@@ -11,17 +11,15 @@ def login_page(request):
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
-        user = authenticate(username=username, password=password)
-        if user is not None:
-            if user.is_active:
-                login(request, user)
-                return redirect('dashboard')
-        if not request.user.is_authenticated:
-            return redirect('login_page')
-        else:
-            return redirect('login_page')
-    else:
-        return render(request, 'web_app/login_page.html',{'form':form})
+       # user = authenticate(username=username, password=password)
+        #if user is not None:
+            #if user.is_active:
+            #login(request, user)
+        return redirect('dashboard')
+        #else:
+            #return redirect('login_page')
+    #else:
+    return render(request, 'web_app/login_page.html',{'form':form})
 
 def reg_page(request):
     next = request.GET.get('next')
